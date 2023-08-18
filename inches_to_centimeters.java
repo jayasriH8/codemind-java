25acf667af27dcf457ccaacf6e687cd3e4302f91
jayasriH8 <119723600+jayasriH8@users.noreply.github.com>
2023-08-18 12:49:14 +0530

    import java.util.Scanner; public class Cm {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         double h=sc.nextDouble();         double cm;         cm=h*2.54;         System.out.printf("%.2f",cm);     } }

---
+++ inches_to_centimeters.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Cm
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        double h=sc.nextDouble();
+        double cm;
+        cm=h*2.54;
+        System.out.printf("%.2f",cm);
+    }
+}
+
+    
