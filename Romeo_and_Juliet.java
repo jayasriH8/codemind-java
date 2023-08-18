a2b55e03e9b7970f03860e98a26d125dd6fc0836
jayasriH8 <119723600+jayasriH8@users.noreply.github.com>
2023-08-18 13:20:21 +0530

    import java.util.Scanner; public class Cm {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         int y=sc.nextInt();         int z=sc.nextInt();         int n;         n=(x*5+y*10)/z;         System.out.print(n);
        }
    }

---
+++ Romeo_and_Juliet.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Cm
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int z=sc.nextInt();
+        int n;
+        n=(x*5+y*10)/z;
+        System.out.print(n);
+        
+        
+    }
+}
