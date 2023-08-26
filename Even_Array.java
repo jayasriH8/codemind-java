1f1911a0387411116436a9d76b5b45acf32d67f8
jayasriH8 <119723600+jayasriH8@users.noreply.github.com>
2023-08-26 10:43:38 +0530

    import java.util.Scanner; public class cm{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int[] arr=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         int count=0;         for(int i=0;i<n;i++)         {             if (arr[i]%2==0)             {                 count++;             }         }         if(count==n)         {             System.out.println("True");         }         else         {             System.out.println("False");         }     } }

---
+++ Even_Array.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class cm{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            if (arr[i]%2==0)
+            {
+                count++;
+            }
+        }
+        if(count==n)
+        {
+            System.out.println("True");
+        }
+        else
+        {
+            System.out.println("False");
+        }
+    }
+}
